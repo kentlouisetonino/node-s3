5c5a2c8fcf584743937b8f0a9b6c9551cddd0c9a
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-10-13 20:51:30 +0800

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config()
 import express, { Request, Response } from 'express'
 
 import App from './lib/app.lib'
-import UploadRoute from './routes/upload.route'
+import S3Route from './routes/s3.route'
 import Environment from './lib/environment.lib'
 
 App.get('/', (req: Request, res: Response) => {
@@ -15,4 +15,4 @@ App.listen(Environment.PORT, () => {
 
 App.use(express.json())
 App.use(express.urlencoded({ extended: true }))
-App.use('/api/upload', UploadRoute)
+App.use('/api/s3', S3Route)
