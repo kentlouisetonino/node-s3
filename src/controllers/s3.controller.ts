84a9dfd187b06b4f8aed37e3e7e0829e88b94496
Kent Louise Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2022-10-14 06:36:07 +0800

    Add Route for Get Signed URL (#21)

--- src/controllers/s3.controller.ts
+++ src/controllers/s3.controller.ts
@@ -4,11 +4,11 @@ import s3UploadFile from '../lib/s3-upload-file.lib'
 import s3GetSignedURL from '../lib/s3-get-signed-url.lib'
 
 export const S3UploadController = async (req: Request, res: Response) => {
-  const bucketName = req.body.bucketName
-  const bucketRegion = req.body.bucketRegion
-  const bucketAccessKeyId = req.body.bucketAccessKeyId
-  const bucketSecretAccessKey = req.body.bucketSecretAccessKey
-  const file: any = req.file
+  const bucketName = req.body?.bucketName
+  const bucketRegion = req.body?.bucketRegion
+  const bucketAccessKeyId = req.body?.bucketAccessKeyId
+  const bucketSecretAccessKey = req.body?.bucketSecretAccessKey
+  const file: any = req?.file
 
   if (!bucketName) {
     res.send({
@@ -53,6 +53,55 @@ export const S3UploadController = async (req: Request, res: Response) => {
 
       res.send({
         key: s3Object.Key,
+        url: s3ObjectURL,
+      })
+    } catch (error) {
+      res.send({
+        error: `${error}`,
+      })
+    }
+  }
+}
+
+export const S3GetSignedURL = async (req: Request, res: Response) => {
+  const bucketName: any = req.query?.bucketName
+  const bucketRegion: any = req.query?.bucketRegion
+  const bucketAccessKeyId: any = req.query?.bucketAccessKeyId
+  const bucketSecretAccessKey: any = req.query?.bucketSecretAccessKey
+  const key: any = req.query?.key
+
+  if (!bucketName) {
+    res.send({
+      error: 'Bucket name is required.',
+    })
+  } else if (!bucketRegion) {
+    res.send({
+      error: 'Bucket region is required.',
+    })
+  } else if (!bucketAccessKeyId) {
+    res.send({
+      error: 'Bucket access key ID is required.',
+    })
+  } else if (!bucketSecretAccessKey) {
+    res.send({
+      error: 'Bucket secret access key is required.',
+    })
+  } else if (!key) {
+    res.send({
+      error: 'Key is required.',
+    })
+  } else {
+    try {
+      const s3ObjectURL = await s3GetSignedURL({
+        bucketName: bucketName,
+        bucketRegion: bucketRegion,
+        bucketAccessKeyId: bucketAccessKeyId,
+        bucketSecretAccessKey: bucketSecretAccessKey,
+        key: key,
+      })
+
+      res.send({
+        key: key,
         url: s3ObjectURL,
       })
     } catch (error) {
--- src/routes/s3.route.ts
+++ src/routes/s3.route.ts
@@ -1,7 +1,11 @@
 import Router from '../lib/router.lib'
 import MulterUploadFile from '../lib/multer-upload-file.lib'
-import { S3UploadController } from '../controllers/s3.controller'
+import {
+  S3UploadController,
+  S3GetSignedURL,
+} from '../controllers/s3.controller'
 
 Router.post('/upload', MulterUploadFile.single('file'), S3UploadController)
+Router.get('/get-url', S3GetSignedURL)
 
 export default Router
