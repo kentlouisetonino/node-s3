5104e9a2c1a5cf763e31d29b84ff46bbf5fb8722
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-10-14 13:59:50 +0800

    Create Delete API

--- src/controllers/s3.controller.ts
+++ src/controllers/s3.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 import s3UploadFile from '../lib/s3-upload-file.lib'
 import s3GetSignedURL from '../lib/s3-get-signed-url.lib'
+import s3DeleteFile from '../lib/s3-delete-file.lib'
 
 export const S3UploadController = async (req: Request, res: Response) => {
   const bucketName = req.body?.bucketName
@@ -52,11 +53,13 @@ export const S3UploadController = async (req: Request, res: Response) => {
       })
 
       res.send({
+        statusCode: 200,
         key: s3Object.Key,
         url: s3ObjectURL,
       })
     } catch (error) {
       res.send({
+        statusCode: 500,
         error: `${error}`,
       })
     }
@@ -101,11 +104,65 @@ export const S3GetSignedURL = async (req: Request, res: Response) => {
       })
 
       res.send({
-        key: key,
-        url: s3ObjectURL,
+        statusCode: 200,
+        key: s3ObjectURL,
       })
     } catch (error) {
       res.send({
+        statusCode: 500,
+        error: `${error}`,
+      })
+    }
+  }
+}
+
+export const S3DeleteFile = async (req: Request, res: Response) => {
+  const bucketName: any = req.body?.bucketName
+  const bucketRegion: any = req.body?.bucketRegion
+  const bucketAccessKeyId: any = req.body?.bucketAccessKeyId
+  const bucketSecretAccessKey: any = req.body?.bucketSecretAccessKey
+  const key: any = req.body?.key
+
+  if (!bucketName) {
+    res.send({
+      error: 'Bucket name is required.',
+    })
+  } else if (!bucketRegion) {
+    res.send({
+      error: 'Bucket region is required.',
+    })
+  } else if (!bucketAccessKeyId) {
+    res.send({
+      error: 'Bucket access key ID is required.',
+    })
+  } else if (!bucketSecretAccessKey) {
+    res.send({
+      error: 'Bucket secret access key is required.',
+    })
+  } else if (!key) {
+    res.send({
+      error: 'Key is required.',
+    })
+  } else {
+    try {
+      const test = await s3DeleteFile({
+        bucketName: bucketName,
+        bucketRegion: bucketRegion,
+        bucketAccessKeyId: bucketAccessKeyId,
+        bucketSecretAccessKey: bucketSecretAccessKey,
+        key: key,
+      })
+
+      console.log(test)
+
+      res.send({
+        statusCode: 200,
+        key: key,
+        message: `Successfully deleted the object.`,
+      })
+    } catch (error) {
+      res.send({
+        statusCode: 500,
         error: `${error}`,
       })
     }
--- src/routes/s3.route.ts
+++ src/routes/s3.route.ts
@@ -3,9 +3,11 @@ import MulterUploadFile from '../lib/multer-upload-file.lib'
 import {
   S3UploadController,
   S3GetSignedURL,
+  S3DeleteFile,
 } from '../controllers/s3.controller'
 
 Router.post('/upload', MulterUploadFile.single('file'), S3UploadController)
-Router.get('/get-url', S3GetSignedURL)
+Router.get('/url', S3GetSignedURL)
+Router.post('/delete', S3DeleteFile)
 
 export default Router
