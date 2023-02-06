0e2c010f91e29aa821345fb042c856092c3ada7a
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-02-06 17:25:44 +0800

    add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { Request, Response, json, urlencoded } from 'express'
-
+import morgan from 'morgan'
 import app from './lib/app'
 import environment from './lib/environment'
 import S3Route from './routes/S3Route'
+
+app.use(morgan('tiny'))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend server is online.')
 })
 
 app.listen(environment.PORT, () => {
-  console.log(`Server is running in port ${environment.PORT}`)
+  console.log(`Server is running in http://localhost:${environment.PORT}`)
 })
 
 app.use(json())
