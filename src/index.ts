6c3c17a8b2927c242df37e624134252e62917773
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-02-06 18:45:29 +0800

    home page

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { Request, Response, json, urlencoded } from 'express'
+import express, { Request, Response, json, urlencoded } from 'express'
 import morgan from 'morgan'
 import app from './lib/app'
 import environment from './lib/environment'
 import S3Route from './routes/S3Route'
 
 app.use(morgan('tiny'))
+app.use(express.static('public'))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Backend server is online.')
+  res.sendFile('index.html', { root: 'public' })
 })
 
 app.listen(environment.PORT, () => {
