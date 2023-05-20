df544f9952c8e0c34b02f09478f0f494d66cc372
Kent Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-05-20 20:12:47 +0800

    feat: refactor S3Service (#49)

--- src/services/S3Service.ts
+++ src/services/S3Service.ts
@@ -8,14 +8,14 @@ import {
 export default class S3Service {
   /**
    * Use this method if to upload a file.
-   * @param bucketName The name of the bucket used in S3.
-   * @param bucketRegion The specific region where a specific S3 is located (e.g. ap-southeast-1).
-   * @param bucketAccessKeyId The unique identifier that is used to authenticate the role when making API calls to S3.
-   * @param bucketSecretAccessKey Used in conjunction with the access key ID to sign API requests and prove the identity of the requester.
-   * @param fileName The name of the file.
-   * @param fileBuffer The temporary storage location that holds the contents of the file until it can be saved to a desired destination, such as storage service or a location file system.
-   * @param fileEncoding Refers to the character encoding used for the file part of the request.
-   * @param fileContentType Provides information about the type and format of the data being transmitted.
+   * @param bucketName string
+   * @param bucketRegion string
+   * @param bucketAccessKeyId string
+   * @param bucketSecretAccessKey string
+   * @param fileName string
+   * @param fileBuffer Buffer
+   * @param fileEncoding string
+   * @param fileContentType string
    */
   static uploadFile({
     bucketName,
@@ -45,6 +45,14 @@ export default class S3Service {
     return s3.upload(params).promise();
   }
 
+  /**
+   *
+   * @param bucketName string
+   * @param bucketRegion string
+   * @param bucketAccessKeyId string
+   * @param bucketSecretAccessKey string
+   * @param key string
+   */
   static deleteFile({
     bucketName,
     bucketRegion,
@@ -67,6 +75,14 @@ export default class S3Service {
       .promise();
   }
 
+  /**
+   *
+   * @param bucketName string
+   * @param bucketRegion string
+   * @param bucketAccessKeyId string
+   * @param bucketSecretAccessKey string
+   * @param key string
+   */
   static getSignedURL({
     bucketName,
     bucketRegion,
