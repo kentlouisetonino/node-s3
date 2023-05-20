9baf952971eb8c935c792ae4d93a22edfa581855
Kent Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-05-20 22:58:58 +0800

    feat: add comment in memoryStorage class (#51)

--- src/services/MulterService.ts
+++ src/services/MulterService.ts
@@ -1,5 +1,9 @@
 import multer from 'multer';
 
 export default class MulterService {
+  /**
+   * This is a middleware configured to store files in
+   * memory as Buffer objects.
+   */
   static memoryStorage = multer({ storage: multer.memoryStorage() });
 }
