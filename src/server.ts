6af2ec5fe780953434cd09954fd759cbadeb53df
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-20 16:09:06 +0800

    convert app instance to class based

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
 import express, { Request, Response, json, urlencoded } from 'express';
 import morgan from 'morgan';
-import app from './libs/app';
 import environment from './libs/environment';
+import ExpressService from './libs/services/ExpressService';
 import S3Route from './routes/S3Route';
 
 // * Server listener.
-app.listen(environment.PORT, () => {
+ExpressService.app.listen(environment.PORT, () => {
   console.log(`Server is running in http://localhost:${environment.PORT}`);
 });
 
 // * Server middlewares.
-app.use(morgan('tiny'));
-app.use(express.static('public'));
-app.use(json());
-app.use(urlencoded({ extended: true }));
+ExpressService.app.use(morgan('tiny'));
+ExpressService.app.use(express.static('public'));
+ExpressService.app.use(json());
+ExpressService.app.use(urlencoded({ extended: true }));
 
 // * Server root endpoints.
-app.get('/', (_: Request, res: Response) => {
+ExpressService.app.get('/', (_: Request, res: Response) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
 // * Server S3 endpoints.
-app.use('/api/s3', S3Route);
+ExpressService.app.use('/api/s3', S3Route);
