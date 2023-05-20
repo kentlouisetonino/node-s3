1628a63c055f6ce7ea2ddcbda430d1add42cbba4
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-20 16:18:57 +0800

    add a new line

--- src/routes/S3Route.ts
+++ src/routes/S3Route.ts
@@ -7,11 +7,13 @@ import ExpressService from '../libs/services/ExpressService';
 import MulterService from '../libs/services/MulterService';
 
 ExpressService.router.get('/url', S3GetSignedURL);
+
 ExpressService.router.post(
   '/upload',
   MulterService.memoryStorage.single('file'),
   S3Upload
 );
+
 ExpressService.router.post(
   '/delete',
   MulterService.memoryStorage.fields([]),
