0d79578311a3cf9e090c708f6daf6d518b4b78ee
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-20 16:17:55 +0800

    move router to ExpressService

--- src/libs/services/ExpressService.ts
+++ src/libs/services/ExpressService.ts
@@ -2,4 +2,5 @@ import express from 'express';
 
 export default class ExpressService {
   static app = express();
+  static router = express.Router();
 }
--- src/routes/S3Route.ts
+++ src/routes/S3Route.ts
@@ -3,11 +3,19 @@ import {
   S3GetSignedURL,
   S3Upload,
 } from '../controllers/S3Controller';
-import router from '../libs/router';
+import ExpressService from '../libs/services/ExpressService';
 import MulterService from '../libs/services/MulterService';
 
-router.get('/url', S3GetSignedURL);
-router.post('/upload', MulterService.memoryStorage.single('file'), S3Upload);
-router.post('/delete', MulterService.memoryStorage.fields([]), S3DeleteFile);
+ExpressService.router.get('/url', S3GetSignedURL);
+ExpressService.router.post(
+  '/upload',
+  MulterService.memoryStorage.single('file'),
+  S3Upload
+);
+ExpressService.router.post(
+  '/delete',
+  MulterService.memoryStorage.fields([]),
+  S3DeleteFile
+);
 
-export default router;
+export default ExpressService.router;
