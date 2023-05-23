c86413ae286f33a766a3575fe4fde37e3ae12752
Kent Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-05-23 22:37:39 +0800

    feat: remove mongodb (#59)
    * remove mongodb
    
    * add mongodb URI
    
    * modify readme

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,11 @@ require('dotenv').config();
 import express, { Request, Response, json, urlencoded } from 'express';
 import mongoose, { ConnectOptions } from 'mongoose';
 import morgan from 'morgan';
-import ColorService from './libs/services/ColorService';
-import EnvironmentService from './libs/services/EnvironmentService';
-import ExpressService from './libs/services/ExpressService';
-import { ColorEnum } from './libs/services/types';
 import DefaultRoute from './routes/DefaultRoute';
-import MongoDBRoute from './routes/MongoDBRoute';
+import ColorService from './services/ColorService';
+import EnvironmentService from './services/EnvironmentService';
+import ExpressService from './services/ExpressService';
+import { ColorEnum } from './services/types';
 
 // * Get the express application instance.
 const app = ExpressService.app;
@@ -36,11 +35,10 @@ app.use(express.static('public'));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-// * Server root endpoints.
+// * Server root endpoint.
 app.get('/', (_: Request, res: Response) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
 // * Server endpoints.
 app.use('/api/default/s3', DefaultRoute);
-app.use('/api/mongodb/s3', MongoDBRoute);
