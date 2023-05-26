6e7ed54c46a891de120ab0c6e55f44c74b39f202
Kent Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2023-05-26 16:31:27 +0800

    feat: separate app and server (#62)

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
 require('dotenv').config();
-import express, { Request, Response, json, urlencoded } from 'express';
-import mongoose, { ConnectOptions } from 'mongoose';
-import morgan from 'morgan';
-import S3Route from './routes/S3Route';
+import app from './app';
 import ColorService from './services/ColorService';
 import EnvironmentService from './services/EnvironmentService';
-import ExpressService from './services/ExpressService';
 import { ColorEnum } from './services/types';
-
-// * Get the express application instance.
-const app = ExpressService.app;
 
 // * Server listener.
 app.listen(EnvironmentService.PORT, () => {
@@ -18,27 +11,3 @@ app.listen(EnvironmentService.PORT, () => {
     `Server is running in http://localhost:${EnvironmentService.PORT}`
   );
 });
-
-// * Connect to mongodb.
-mongoose
-  .connect(EnvironmentService.MONGODB_URI ?? '', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions)
-  .then(() => {
-    ColorService.logText(ColorEnum.FgYellow, `Server is connected to MongoD`);
-  });
-
-// * Server middlewares.
-app.use(morgan('tiny'));
-app.use(express.static('public'));
-app.use(json());
-app.use(urlencoded({ extended: true }));
-
-// * Server root endpoint.
-app.get('/', (_: Request, res: Response) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-// * Server other endpoints.
-app.use('/api/s3', S3Route);
