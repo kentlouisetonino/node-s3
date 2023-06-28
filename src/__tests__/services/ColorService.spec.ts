9cee63837719d2c9fbd7edb2ff8811bbacd532fb
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-06-28 16:09:48 +0800

    update imports

--- src/__tests__/services/ColorService.spec.ts
+++ src/__tests__/services/ColorService.spec.ts
@@ -1,5 +1,4 @@
-import ColorService from '../../services/ColorService';
-import { ColorEnum } from '../../services/types';
+import ColorService, { ColorEnum } from '../../services/ColorService';
 
 describe('[ src/services/ColorService ]', () => {
   test('a. It should return a type string.', (done) => {
