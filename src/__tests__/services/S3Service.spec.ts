2978adc03afbc1592d56d7543d1a0c7cc22a669f
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-07-06 21:58:19 +0800

    add unit test

--- src/__tests__/services/S3Service.spec.ts
+++ src/__tests__/services/S3Service.spec.ts
@@ -1,7 +1,22 @@
-// * TODO: Implement a unit test for the S3 service.
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+import S3Service from '../../services/S3Service';
+
 describe('[ S3Service ]', () => {
-  test('a. TODO', (done) => {
-    expect('unit-test').toBe('unit-test');
+  test('a. It should call the uploadFile method.', (done) => {
+    const uploadFile = S3Service.uploadFile;
+    expect(typeof uploadFile).toBe('function');
+    done();
+  });
+
+  test('b. It should call the deleteFile method.', (done) => {
+    const deleteFile = S3Service.deleteFile;
+    expect(typeof deleteFile).toBe('function');
+    done();
+  });
+
+  test('c. It should call the getSignedURL method.', (done) => {
+    const getSignedURL = S3Service.getSignedURL;
+    expect(typeof getSignedURL).toBe('function');
     done();
   });
 });
