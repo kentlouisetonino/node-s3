8655f944427448903622b30af87540f299db2513
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-24 05:01:38 +0800

    Remove unnecessary comments.

--- src/routes/RootRoute.ts
+++ src/routes/RootRoute.ts
@@ -4,7 +4,6 @@ import ExpressService from '../services/ExpressService';
 // Get the express router instance.
 const router = ExpressService.router;
 
-// Default endpoints.
 router.get('/', RootController.home);
 
 export default router;
